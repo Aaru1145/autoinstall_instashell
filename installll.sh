e1724aec9b04f01c13be6ad8dfd8beb869a3d142
darksec <37667454+r3x07@users.noreply.github.com>
2020-09-10 15:40:41 +0500

    for friends so ignore

---
+++ installll.sh
@@ -0,0 +1,15 @@
+pkg install openssl curly tor git
+mkdir adam
+cd adam
+echo "made by adam )00))"
+git clone https://github.com/maxrooted/instashell
+cd instashell && chmod 777 *
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '17d' instashell.sh
+sed -i '52d' instashell.sh
+bash instashell.sh
